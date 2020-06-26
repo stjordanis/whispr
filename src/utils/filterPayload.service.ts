3cf13d6d6c3267bb68718049009e0fd823c7db2e
Jérémie Marniquet Fabre <jeremiem@theodo.fr>
2020-06-26 10:44:08 +0200

    refactor(filter): Move filter to utils

---
+++ src/utils/filterPayload.service.ts
@@ -0,0 +1,51 @@
+import { isEqual } from 'lodash';
+
+export const matches = (
+  filterValue: unknown,
+  elementValue: unknown,
+): boolean => {
+  if (Array.isArray(filterValue)) {
+    return filterValue.some((value) => matches(value, elementValue));
+  }
+
+  return isEqual(filterValue, elementValue);
+};
+
+export const payloadMatchesNestedValue = (
+  keyArray: string[],
+  nestedValue: unknown,
+  payload: Record<string, unknown>,
+) => {
+  let currentObj: unknown = payload;
+
+  while (keyArray.length > 1) {
+    const key = keyArray.shift();
+
+    if (!currentObj[key]) {
+      return false;
+    }
+    currentObj = currentObj[key];
+  }
+
+  return matches(nestedValue, currentObj[keyArray.shift()]);
+};
+
+export const filterPayload = (
+  filter: Record<string, unknown>,
+  payload: unknown,
+): boolean => {
+  return Object.keys(filter).every((key) => {
+    const filterValue = filter[key];
+
+    if (filterValue === undefined || payload === undefined) {
+      return false;
+    }
+
+    const keyArray = key.split('.');
+    if (keyArray.length !== 1) {
+      return this.payloadMatchesNestedValue(keyArray, filterValue, payload);
+    }
+
+    return matches(filterValue, payload[key]);
+  });
+};
--- tests/unit/utils/filterPayload.service.spec.ts
+++ tests/unit/utils/filterPayload.service.spec.ts
@@ -1,90 +1,90 @@
-import { WhispResolver } from '../../../src/whisp/whisp.resolver';
+import { filterPayload, payloadMatchesNestedValue } from '../../../src/utils/filterPayload.service';
 
-describe('WhispResolver', () => {
+describe('WhispFilter', () => {
   describe('filter', () => {
     it('should return true if no filter was provided', () => {
-      expect(WhispResolver.filter(undefined, {})).toEqual(true);
+      expect(filterPayload(undefined, {})).toEqual(true);
     });
     it('should return false if there is no value attach to the filter key', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: undefined },
         { aKey: 'aValue' },
       )).toEqual(false);
     });
     it('should return false if there is no payload', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: 'a value' },
         undefined,
       )).toEqual(false);
     });
     it('should return false if there is no corresponding key in the payload', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: 'aValue' },
         { notTheSameKey: 'aValue' },
       )).toEqual(false);
     });
     it('should return true if all conditions are true', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: 'aValue', anotherKey: 'anotherValue' },
         { aKey: 'aValue', anotherKey: 'anotherValue' },
       )).toEqual(true);
     });
     it('should return false if all conditions are not true', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: 'aValue', anotherKey: 'anotherValue' },
         { aKey: 'aValue', anotherKey: 'notTheExpectedValue' },
       )).toEqual(false);
     });
     it('should go recursive and return true if all conditions are true', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: { anotherKey: 'aValue' } },
         { aKey: { anotherKey: 'aValue' } },
       )).toEqual(true);
     });
     it('should go recursive and return false if a condition is not met', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: { anotherKey: 'aValue' } },
         { aKey: { anotherKey: 'anotherValue' } },
       )).toEqual(false);
     });
     it('should return true if at least one condition of the array is met', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: ['valueA', 'valueB', 'valueC'] },
         { aKey: 'valueB' },
       )).toEqual(true);
     });
     it('should return false if not a condition of the array is met', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: ['valueA', 'valueB', 'valueC'] },
         { aKey: 'valueD' },
       )).toEqual(false);
     });
     it('should go recursive and return true if at least a condition of the array is met', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: [{ anotherKey: 'anotherValue' }, { yetAnotherKey: 'yetAnotherValue' }, 'valueC'] },
         { aKey: { yetAnotherKey: 'yetAnotherValue' } },
       )).toEqual(true);
     });
     it('should go recursive and return false if at least a condition of the array is met', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { aKey: [{ anotherKey: 'anotherValue' }, { yetAnotherKey: 'yetAnotherValue' }, 'valueC'] },
         { aKey: { yetAnotherKey: 'notTheExpectedValue' } },
       )).toEqual(false);
     });
     it('should return true if nested value matches', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { 'aKey.anotherKey': 'valueA' },
         { aKey: { anotherKey: 'valueA' } },
       )).toEqual(true);
     });
     it('should return true if several nested values match', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { 'key1.key2': 'valueA', 'key1.key3': 'valueB' },
         { key1: { key2: 'valueA', key3: 'valueB' } },
       )).toEqual(true);
     });
     it('should not return an object if it doesn\'t match fully', () => {
-      expect(WhispResolver.filter(
+      expect(filterPayload(
         { key1: { key2: 'ValueA' } },
         { key1: { key2: 'ValueA', key3: 'ValueB' } },
       )).toEqual(false);
@@ -93,28 +93,28 @@ describe('WhispResolver', () => {
 
   describe('payloadMatchesNestedValue', () => {
     it('should match', () => {
-      expect(WhispResolver.payloadMatchesNestedValue(
+      expect(payloadMatchesNestedValue(
         ['att1', 'att2', 'att3'],
         'ValueA',
         { att1: { att2: { att3: 'ValueA' } } },
       )).toEqual(true);
     });
     it('should not match if value differs', () => {
-      expect(WhispResolver.payloadMatchesNestedValue(
+      expect(payloadMatchesNestedValue(
         ['att1', 'att2', 'att3'],
         'ValueA',
         { att1: { att2: { att3: 'ValueB' } } },
       )).toEqual(false);
     });
     it('should not match if property is not there', () => {
-      expect(WhispResolver.payloadMatchesNestedValue(
+      expect(payloadMatchesNestedValue(
         ['att1', 'att2', 'att3'],
         'ValueA',
         { att1: { att2: 'ValueA' } },
       )).toEqual(false);
     });
     it('should match if there are other properties', () => {
-      expect(WhispResolver.payloadMatchesNestedValue(
+      expect(payloadMatchesNestedValue(
         ['att1', 'att2', 'att3'],
         'ValueA',
         { att1: { att2: { att3: 'ValueA' } }, att4: 'ValueB' },
